63b9083954f6b62695da05e89c5a745960a997f6
sgjy <1213353801@qq.com>
2026-07-14 14:06:41 +0800

    ✨ :Grid案例调整

--- examples/Grid/Basic.tsx
+++ examples/Grid/Basic.tsx
@@ -1,26 +1,70 @@
 import React from 'react';
-import { Row, Col } from '../../src';
+import {Row, Col} from '../../src';
 
 export default () => (
-  <>
-    <Row>
-      <Col span={12}>
-        <div style={{ background: '#1677ff', color: '#fff', textAlign: 'center', padding: '16px 8px', borderRadius: '6px', minHeight: '40px' }}>col-12</div>
-      </Col>
-      <Col span={12}>
-        <div style={{ background: '#1677ff', color: '#fff', textAlign: 'center', padding: '16px 8px', borderRadius: '6px', minHeight: '40px' }}>col-12</div>
-      </Col>
-    </Row>
-    <Row>
-      <Col span={8}>
-        <div style={{ background: '#1677ff', color: '#fff', textAlign: 'center', padding: '16px 8px', borderRadius: '6px', minHeight: '40px' }}>col-8</div>
-      </Col>
-      <Col span={8}>
-        <div style={{ background: '#1677ff', color: '#fff', textAlign: 'center', padding: '16px 8px', borderRadius: '6px', minHeight: '40px' }}>col-8</div>
-      </Col>
-      <Col span={8}>
-        <div style={{ background: '#1677ff', color: '#fff', textAlign: 'center', padding: '16px 8px', borderRadius: '6px', minHeight: '40px' }}>col-8</div>
-      </Col>
-    </Row>
-  </>
+    <>
+        <div style={{ marginBottom: 20 }}>
+            <Row>
+                <Col span={12}>
+                    <div style={{
+                        background: '#1677ff',
+                        color: '#fff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px'
+                    }}>col-12
+                    </div>
+                </Col>
+                <Col span={12}>
+                    <div style={{
+                        background: '#1677ff',
+                        color: '#fff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px'
+                    }}>col-12
+                    </div>
+                </Col>
+            </Row>
+        </div>
+        <div>
+            <Row>
+                <Col span={8}>
+                    <div style={{
+                        background: '#1677ff',
+                        color: '#fff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px'
+                    }}>col-8
+                    </div>
+                </Col>
+                <Col span={8}>
+                    <div style={{
+                        background: '#1677ff',
+                        color: '#fff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px'
+                    }}>col-8
+                    </div>
+                </Col>
+                <Col span={8}>
+                    <div style={{
+                        background: '#1677ff',
+                        color: '#fff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px'
+                    }}>col-8
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    </>
 );
--- examples/Grid/Gutter.tsx
+++ examples/Grid/Gutter.tsx
@@ -1,19 +1,163 @@
 import React from 'react';
-import { Row, Col } from '../../src';
+import {Row, Col} from '../../src';
 
 export default () => (
-  <Row gutter={16}>
-    <Col span={6}>
-      <div style={{ background: '#e6f7ff', border: '1px solid #91d5ff', textAlign: 'center', padding: '16px 8px', borderRadius: '6px', minHeight: '40px', color: '#1677ff' }}>col-6</div>
-    </Col>
-    <Col span={6}>
-      <div style={{ background: '#e6f7ff', border: '1px solid #91d5ff', textAlign: 'center', padding: '16px 8px', borderRadius: '6px', minHeight: '40px', color: '#1677ff' }}>col-6</div>
-    </Col>
-    <Col span={6}>
-      <div style={{ background: '#e6f7ff', border: '1px solid #91d5ff', textAlign: 'center', padding: '16px 8px', borderRadius: '6px', minHeight: '40px', color: '#1677ff' }}>col-6</div>
-    </Col>
-    <Col span={6}>
-      <div style={{ background: '#e6f7ff', border: '1px solid #91d5ff', textAlign: 'center', padding: '16px 8px', borderRadius: '6px', minHeight: '40px', color: '#1677ff' }}>col-6</div>
-    </Col>
-  </Row>
+    <>
+        <div style={{marginBottom: 20}}>
+            <Row gutter={26}>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+            </Row>
+        </div>
+        <div style={{marginBottom: 20}}>
+            <Row gutter={16}>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+            </Row>
+        </div>
+        <div style={{marginBottom: 20}}>
+            <Row gutter={10}>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+                <Col span={6}>
+                    <div style={{
+                        background: '#e6f7ff',
+                        border: '1px solid #91d5ff',
+                        textAlign: 'center',
+                        padding: '16px 8px',
+                        borderRadius: '6px',
+                        minHeight: '40px',
+                        color: '#1677ff'
+                    }}>col-6
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    </>
 );
